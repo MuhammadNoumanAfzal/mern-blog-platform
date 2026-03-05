cf60fa0d60bb0cef8fae66fb602606cb192f9ae9
MuhammadNoumanAfzal <mnoumanafzal4@gmail.com>
2026-03-05 18:06:01 +0500

    Enhance siteController and views: implement news fetching with categories, update routing for articles, and create a reusable loop partial for article rendering.

--- controller/siteController.js
+++ controller/siteController.js
@@ -6,20 +6,64 @@ import userModel from "../models/User.js";
 import commentModel from "../models/Comments.js";
 
 const index = async (req, res) => {
-  res.render("index");
+  const news= await newsModel.find().populate("category", {categoryName:1, "slug":1,}).populate("author", "fullName").sort({ createdAt: -1 })
+
+  const categoriesInUse = await newsModel.distinct("category");
+  const categories = await categoryModel.find({ _id: { $in: categoriesInUse } });
+  // res.json(news)
+  
+  res.render("index", {news, categories} );
 };
 const articleByCategories = async (req, res) => {
-  res.render("category");
+  const category=await categoryModel.findOne({slug: req.params.name})
+  if(!category){
+    return res.status(404).send("Category not found")
+  }
+
+
+  const news= await newsModel.find({ category: category._id }).populate("category", {categoryName:1, "slug":1,}).populate("author", "fullName").so rt({ createdAt: -1 })
+
+  const categoriesInUse = await newsModel.distinct("category");
+  const categories = await categoryModel.find({ _id: { $in: categoriesInUse } });
+  // res.json(news)
+  
+  res.render("category", {news, categories} );
 };
+
+ 
 const singleArticle = async (req, res) => {
-  res.render("single");
+  const singleNews= await newsModel.findById(req.params.id).populate("category", {categoryName:1, "slug":1,}).populate("author", "fullName").sort({ createdAt: -1 })
+
+  const categoriesInUse = await newsModel.distinct("category");
+  const categories = await categoryModel.find({ _id: { $in: categoriesInUse } });
+  // res.json(news)
+  
+  res.render("single", {singleNews, categories} );
 };
+
+ 
 const search = async (req, res) => {
-  res.render("search");
+  const news= await newsModel.find().populate("category", {categoryName:1, "slug":1,}).populate("author", "fullName").sort({ createdAt: -1 })
+
+  const categoriesInUse = await newsModel.distinct("category");
+  const categories = await categoryModel.find({ _id: { $in: categoriesInUse } });
+  // res.json(news)
+  
+  res.render("search", {news, categories} );
 };
+
+  
 const author = async (req, res) => {
-  res.render("author");
+  const news= await newsModel.find({author: req.params.id}).populate("category", {categoryName:1, "slug":1,}).populate("author", "fullName").sort({ createdAt: -1 })
+
+  const categoriesInUse = await newsModel.distinct("category");
+  const categories = await categoryModel.find({ _id: { $in: categoriesInUse } });
+  // res.json(news)
+  
+  res.render("author", {news, categories} );
 };
+
+ 
 const addComment = async (req, res) => {};
 
 export {
